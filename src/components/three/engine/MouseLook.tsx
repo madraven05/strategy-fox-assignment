a39b36f428db1849ec123b0f0d1b0f88443cfcfe
pranshu-kumar <pranshukumargond05@gmail.com>
2024-12-17 15:08:21 +0530

    logic removed pointerlock

--- src/components/three/engine/MouseLook.tsx
+++ src/components/three/engine/MouseLook.tsx
@@ -15,8 +15,7 @@ const MouseLook: React.FC = () => {
 
       yaw.current -= movementX * sensitivity;
       pitch.current -= movementY * sensitivity;
-    
-      // clamp x rotation to -pi/2 to pi/2
+
       pitch.current = Math.max(
         -Math.PI / 2,
         Math.min(Math.PI / 2, pitch.current)
@@ -25,16 +24,10 @@ const MouseLook: React.FC = () => {
       camera.rotation.set(pitch.current, yaw.current, 0, "YXZ");
     };
 
-    const onClick = () => {
-      gl.domElement.requestPointerLock();
-    };
-
     document.addEventListener("mousemove", onMouseMovement);
-    document.addEventListener("click", onClick);
 
     return () => {
       document.removeEventListener("mousemove", onMouseMovement);
-      document.removeEventListener("click", onClick);
     };
   }, [camera, gl]);
 
