84c7922d690f491a217c02281b3b6c8f750f4f68
pranshu-kumar <pranshukumargond05@gmail.com>
2024-12-17 22:34:45 +0530

    environment added

--- src/components/three/scene/MainScene.tsx
+++ src/components/three/scene/MainScene.tsx
@@ -4,7 +4,7 @@ import GoldenBayEnvironment from "./environment/GoldenBayEnvironment";
 import { PinkRoom } from "../objects/PinkRoom";
 import SceneLoader from "../SceneLoader";
 import Locomotion from "../engine/Locomotion";
-import { Physics, RigidBody } from "@react-three/rapier";
+import { Physics } from "@react-three/rapier";
 
 const MainScene: React.FC = () => {
   return (
@@ -15,18 +15,12 @@ const MainScene: React.FC = () => {
         <Locomotion />
         {/* <OrbitControls /> */}
         <Suspense fallback={<SceneLoader />}>
-          {/* <GoldenBayEnvironment /> */}
+          <GoldenBayEnvironment />
           <Physics debug gravity={[0, -98, 0]}>
             <group rotation={[0, Math.PI / 10, 0]} position={[800, -400, -250]}>
               <PinkRoom scale={30} />
               
             </group>
-            <RigidBody type="dynamic" colliders="ball" restitution={1} friction={0.4}>
-                <mesh position={[850, -250, -250]}>
-                  <sphereGeometry args={[50, 64, 64]} />
-                  <meshStandardMaterial color={"royalblue"}/>
-                </mesh>
-              </RigidBody>
           </Physics>
         </Suspense>
       </Canvas>
