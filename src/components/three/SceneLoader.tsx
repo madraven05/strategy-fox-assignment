3d70ce9b2e01cc871636cb338cec68dd49baae88
pranshu-kumar <pranshukumargond05@gmail.com>
2024-12-18 11:56:36 +0530

    scene loader designed

--- src/components/three/SceneLoader.tsx
+++ src/components/three/SceneLoader.tsx
@@ -2,12 +2,20 @@ import { Html, useProgress } from "@react-three/drei";
 import React from "react";
 
 const SceneLoader: React.FC = () => {
-  const {progress} = useProgress();
+  const { progress } = useProgress();
   return (
     <Html center>
+      {progress <= 100 ? (
         <div>
-            {progress < 100 ? <h1>Loading...{progress.toFixed(2)}%</h1> : null}
+          <div className="bg-black/10 w-40 h-40 p-2 rounded-lg shadow-lg flex flex-col gap-3 items-center justify-center">
+            <p className="font-semibold">Loading scene</p>
+            <p className="text-sm">{progress.toFixed(2)}%</p>
+            <div className={`w-full h-2 rounded-md bg-gray-300 flex`}>
+              <div className={`h-full bg-teal-500 rounded-md`} style={{width: `${progress.toFixed(2)}%`}} />
+            </div>
+          </div>
         </div>
+      ) : null}
     </Html>
   );
 };
